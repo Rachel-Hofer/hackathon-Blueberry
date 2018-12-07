f1ff4c6a8ba4365abfe61ecbcca87d3794074240
Rachel Hofer <rhofer3@gmail.com>
2018-12-07 16:52:16 -0500

    done

---
+++ app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() { // when the document is loaded
+
+    //TOP carasoul right click
+    $("#right_button").click(function() {
+    
+    })
+    
+    //BOTTOM carasoul right click
+    $("#right_button_lower").click(function() {
+      
+    })
+    
+    //TOP carasoul left click
+    $("#left_button").click(function() {
+      
+    })
+    
+    //BOTTOM carasoul left click
+    $("#left_button_lower").click(function() {
+          
+    })
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }) // end of JQuery wrap
